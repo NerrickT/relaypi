259a1d3e1bfc1f374b1f8d910f0a51cc433942b1
Pavilion <vincent_valentine@outlook.com>
2017-08-24 01:09:13 -0500

    Adding in email notification.

--- cron_sync.py
+++ cron_sync.py
@@ -7,6 +7,9 @@ Documentation style: https://www.python.org/dev/peps/pep-0008/
 
 from __future__ import print_function     # Keep Python 2 inline with Python 3
 import os
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
 import time
 import sys  # This allows for user input
 import shutil
@@ -17,10 +20,31 @@ from pathlib import Path  # Need to re-write for python 2
 
 def sync_repo():
     path = "~/GIT/Python/relaypi"
-    #os.chdir(path)
-    os.path.expanduser(path)
+    os.path.expanduser(path) # This allows for us to use the home directory shortcut(~).
     os.system("git pull")
     print("Repo sync has completed.")
+    send_email()
+
+
+def send_email():
+    fromaddr = "raythomas@gmail.com"
+    toaddr = "raythomas@live.com"
+    msg = MIMEMultipart()
+    msg['From'] = fromaddr
+    msg['To'] = toaddr
+    msg['Subject'] = "Sync has completed"
+
+    body = "RelayPi Sync has completed"
+    msg.attach(MIMEText(body, 'plain'))
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(fromaddr, "nofhenvpxqdkfjae")
+    text = msg.as_string()
+    server.sendmail(fromaddr, toaddr, text)
+    server.quit()
+
+
 
 
 def main():
